15689e977cd04df06150a755c294081f40f14753
Joel Löf <jlof@evolution.com>
2023-10-08 16:09:05 +0200

    feat: button for connecting mic

--- src/index.ts
+++ src/index.ts
@@ -8,20 +8,31 @@ const displays = {
   integrated: document.getElementById('integrated')
 } as const
 
+const buttons = {
+  start_song: document.getElementById('start_song') as HTMLButtonElement,
+  connect_mic: document.getElementById('connect_mic') as HTMLButtonElement,
+  disconnect_mic: document.getElementById('disconnect_mic') as HTMLButtonElement,
+} as const
+
+buttons.start_song.addEventListener('click', startSong);
+buttons.connect_mic.addEventListener('click', connect_mic);
+buttons.disconnect_mic.addEventListener('click', disconnect_mic)
+
 async function load (url: string): Promise<AudioBuffer> {
   const response = await fetch(url);
   const arrayBuffer = await response.arrayBuffer();
   return await context.decodeAudioData(arrayBuffer);
 }
 
-async function start (): Promise<void> {
+async function startSong (): Promise<void> {
   await context.resume();
   const source = context.createBufferSource();
   source.connect(context.destination);
   const meter = new LoudnessMeter({
     source,
     modes: ['momentary', 'short_term', 'integrated'],
-    workerUri: 'meter-worker.js',
+    workletUri: 'meter-worklet.js',
+    // workerUri: 'meter-worker.js',
   })
   meter.on('dataavailable', (event) => {
     displays[event.data.mode].textContent = String(event.data.value).slice(0, 5);
@@ -31,5 +42,27 @@ async function start (): Promise<void> {
   source.start();
 }
 
-// start()
-window.addEventListener('click', start, { once: true });
+let mic: MediaStreamAudioSourceNode;
+let meter: LoudnessMeter;
+async function connect_mic (): Promise<void> {
+  await context.resume();
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  mic = context.createMediaStreamSource(stream);
+  meter?.off();
+  meter?.stop();
+  meter = new LoudnessMeter({
+    source: mic,
+    modes: ['momentary', 'short_term', 'integrated'],
+    workerUri: 'meter-worker.js',
+
+  })
+  meter.on('dataavailable', (event) => {
+    displays[event.data.mode].textContent = String(event.data.value).slice(0, 5);
+  })
+  meter.start();
+}
+
+function disconnect_mic (): void {
+  mic?.disconnect();
+
+}
